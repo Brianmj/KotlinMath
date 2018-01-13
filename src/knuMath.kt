aee06f9caffcb00aa49c0e76831a27abf36f2de0
brianmj <spamthiseml@hotmail.com>
2018-01-13 14:58:55 -0500

    Made some changes to Vector2, and added a Vector3 class. They both seem to check out as correct.

--- src/knuMath.kt
+++ src/knuMath.kt
@@ -1,59 +1,52 @@
 import kotlin.math.sqrt
 
-public data class Vec(var xDir: Float = 0.0f, var yDir: Float = 0.0f){
-
-    val asPoint: Point
-    get() { return Point(xDir, yDir)}
-
+public data class Vector2(var xDir: Float = 0.0f, var yDir: Float = 0.0f){
     val length: Float get() =  sqrt((xDir * xDir) + (yDir * yDir))
     val lengthSqr: Float get() = (xDir * xDir) + (yDir * yDir)
-
-
-    val normalized: Vec
-    get(){
-        return this / length
-    }
-
+    val normalized: Vector2 get() = this / length
 }
 
-operator fun Vec.plus(other: Vec): Vec {
-    return Vec(this.xDir + other.xDir, this.yDir + other.yDir)
+operator fun Vector2.plus(other: Vector2): Vector2 {
+    return Vector2(this.xDir + other.xDir, this.yDir + other.yDir)
 }
 
-operator fun Vec.minus(other: Vec): Vec {
-    return Vec(this.xDir - other.xDir, this.yDir - other.yDir)
+operator fun Vector2.minus(other: Vector2): Vector2 {
+    return Vector2(this.xDir - other.xDir, this.yDir - other.yDir)
 }
 
-operator fun Vec.times(scalar: Float): Vec {
-    return Vec(this.xDir * scalar, this.yDir * scalar)
+operator fun Vector2.unaryMinus() = Vector2(-xDir, -yDir)
+
+operator fun Vector2.times(scalar: Float): Vector2 {
+    return Vector2(this.xDir * scalar, this.yDir * scalar)
 }
 
-operator fun Float.times(vec: Vec): Vec {
-    return vec * this
+operator fun Float.times(vector2: Vector2): Vector2 {
+    return vector2 * this
 }
 
-operator fun Vec.div(scalar: Float): Vec {
-    return Vec(this.xDir / scalar, this.yDir / scalar)
+operator fun Vector2.div(scalar: Float): Vector2 {
+    return Vector2(this.xDir / scalar, this.yDir / scalar)
 }
 
-operator fun Float.div(vec: Vec): Vec {
-    return Vec(this/ vec.xDir, this / vec.yDir)
+operator fun Float.div(vector2: Vector2): Vector2 {
+    return Vector2(this/ vector2.xDir, this / vector2.yDir)
 }
 
-
-public data class Point(var x: Float = 0.0f, var y: Float = 0.0f) {
-    fun addVector(vec: Vec): Point {
-        val x1 = x + vec.xDir
-        val y1 = y + vec.yDir
-
-        return Point(x1, y1)
-    }
+public data class Vector3(var xDir: Float = 0.0f, var yDir: Float = 0.0f, var zDir: Float = 0.0f)
+{
+    val length: Float get() = sqrt((xDir * xDir) + (yDir * yDir) + (zDir * zDir))
+    val lengthSqr: Float get() = (xDir * xDir) + (yDir * yDir) + (zDir * zDir)
+    val normalized: Vector3 get() = this / length
 }
 
-operator fun Point.plus(other: Point): Vec {
-    return Vec(this.x + other.x, this.y + other.y)
-}
+operator fun Vector3.plus(other: Vector3) = Vector3(this.xDir + other.xDir, this.yDir + other.yDir, this.zDir + other.zDir)
 
-operator fun Point.minus(other: Point): Vec {
-    return Vec(this.x - other.x, this.y - other.y)
-}
+operator fun Vector3.minus(other: Vector3) = Vector3(this.xDir - other.xDir, this.yDir - other.yDir, this.zDir - other.zDir)
+
+operator fun Vector3.unaryMinus(): Vector3 = Vector3(-this.xDir, -this.yDir, -this.zDir)
+
+operator fun Vector3.times(scalar: Float) = Vector3(this.xDir * scalar, this.yDir * scalar, this.zDir * scalar)
+operator fun Float.times(vector3: Vector3) = vector3 * this
+
+operator fun Vector3.div(scalar: Float) = Vector3(this.xDir / scalar, this.yDir / scalar, this.zDir / scalar)
+operator fun Float.div(vector3: Vector3) = Vector3(this / vector3.xDir, this / vector3.yDir, this / vector3.zDir)
--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,24 @@
+// Testing Vector3 stuff
+fun main(args: Array<String>){
+    val a = Vector3(1.0f, 1.0f, 0.0f)
+    val b = Vector3(1.0f, 2.0f, 4.0f)
 
+    val c = a + b
+    val d = a - b
 
+    println("Length of a is ${a.length}")
+    println("Length square of a is ${a.lengthSqr}")
+    println("a normalized is ${a.normalized}")
+    println(c)
+    println(d)
+    println(-b)
+    println(5f * b)
+    println(b * -5f)
+    println(b / 2f)
+    println(24f / b)
+}
+
+/* Testing Vector2 stuff
 fun main(args: Array<String>){
 
     val p = Point(0f, -1f)
@@ -17,9 +36,9 @@ fun main(args: Array<String>){
 
 
     /*val p1 = Point(1.0f, 0.0f)
-    val v = Vec(2.0f, 3.0f)
+    val v = Vector2(2.0f, 3.0f)
 
-    val p2 = p1.addVector(v)*/
+    val p2 = p1.addVector(v)
 
     /*println(p2)
 
@@ -40,12 +59,9 @@ fun main(args: Array<String>){
     println(normalized.yDir)
     println(normalized.length)*/
 
-    /*val v1 = Vec(4f, 0f)
-    val v2 = Vec(0f, -5f)
+    /*val v1 = Vector2(4f, 0f)
+    val v2 = Vector2(0f, -5f)
 
     val r = v1 - v2
-    println(r)*/
-
-
-
-}
+    println(r)
+} */
